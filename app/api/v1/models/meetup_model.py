f65137845d6eaaef7ac05ac4e4817374635c23cd
Kelvin Bunei <keighb@live.com>
2019-01-24 10:20:08 +0300

    meetup model

---
+++ app/api/v1/models/meetup_model.py
@@ -0,0 +1,16 @@
+from ..utils.utils import generate_id
+from .base_model import Model
+
+meetups = []
+
+class Meetup(Model):
+    """ Model class for the Meetup object """
+
+    def __init__(self):
+        super(Meetup,self).__init__(meetups)
+
+    def save(self, data):
+        """ Function to save new meetup """
+        
+        data['id'] = generate_id(self.collection)
+        return super().save(data)
