928568b2f8b6a0c068a2d79431f2c351ed1c6f48
Kelvin Bunei <keighb@live.com>
2019-01-24 10:21:18 +0300

    question model

---
+++ app/api/v1/models/question_model.py
@@ -0,0 +1,37 @@
+from .base_model import Model
+from ..utils.utils import generate_id
+
+questions = []
+
+class Question(Model):
+    """ Model class for the Question object """
+
+    def __init__(self):
+        super(Question,self).__init__(questions)
+
+    def save(self, data):
+        """ Function to save new question """
+
+        data['id'] = generate_id(self.collection)
+        data['votes'] = 0
+        return super().save(data)
+
+    def upvote(self, question_id):
+        """ Function to upvote question """
+
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']+1
+
+            return question
+
+    def downvote(self, question_id):
+        """ Function to downvote question """
+        
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']-1
+
+            return question
+
+        
