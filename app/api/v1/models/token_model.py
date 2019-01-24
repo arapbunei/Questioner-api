0287daf67fc3f2862ea1eb5514f2656268549075
Kelvin Bunei <keighb@live.com>
2019-01-24 10:22:28 +0300

    token model

---
+++ app/api/v1/models/token_model.py
@@ -0,0 +1,15 @@
+
+revoked_tokens = []
+
+class RevokedTokenModel(object):
+    """ Model class for Revoked tokens identifier """
+
+    def add(self, jti):
+        """ Function to save token identifier """
+
+        revoked_tokens.append(jti)
+
+    def is_blacklisted(self, jti):
+        """ Function to check if token identifier is blacklisted """
+
+        return bool(jti in revoked_tokens)
