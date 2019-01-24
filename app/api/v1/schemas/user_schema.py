fd26df467f8cfaa633632bf4b8f133990a7170e6
Kelvin Bunei <keighb@live.com>
2019-01-24 10:28:35 +0300

    user schema

---
+++ app/api/v1/schemas/user_schema.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields
+from ..utils.validator import email, password, required
+
+class UserSchema(Schema):
+    """ Class to validate schema for User object """
+
+    id = fields.Int(dump_only=True)
+    firstname = fields.Str(required=True, validate=(required))
+    lastname = fields.Str(required=True, validate=(required))
+    othername = fields.Str(required=False)
+    username = fields.Str(required=True, validate=(required))
+    phone_number = fields.Str(required=True, validate=(required))
+    email = fields.Email(required=True, validate=(email))
+    password = fields.Str(required=True, validate=(password))
+    registered_on = fields.DateTime(dump_only=True)
+    modified_on = fields.DateTime(dump_only=True)
+    is_admin = fields.Bool(dump_only=True)
+
